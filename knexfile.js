79eefc36c6492bf15f1bd06149259e6dbfe96a31
Mario Amaya <thefozcat@gmail.com>
2019-05-31 16:06:13 -0700

    configured knex, created users migration

---
+++ knexfile.js
@@ -0,0 +1,57 @@
+// Update with your config settings.
+require('dotenv').config();
+
+const pg = require('pg');
+pg.defaults.ssl = true;
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    useNullAsDefault: true,
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    pool: {
+      min: 0,
+      max: 10
+    }
+  },
+
+  // staging: {
+  //   client: 'pg',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
+  // production: {
+  //   client: 'pg',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
+
+};
